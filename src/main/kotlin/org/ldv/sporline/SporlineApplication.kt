65697719b64d4f90cff29753f0a5684c19f8278b
ayoubkh77 <ayoubkhennouci56@gmail.com>
2025-11-10 09:20:32 +0100

    firstCommit

---
+++ src/main/kotlin/org/ldv/sporline/SporlineApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.sporline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SporlineApplication
+
+fun main(args: Array<String>) {
+    runApplication<SporlineApplication>(*args)
+}
---
+++ src/test/kotlin/org/ldv/sporline/SporlineApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ldv.sporline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SporlineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
