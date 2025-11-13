feb53e7ae30bc70bbddc3ac229105d55cee77e1e
ayoubkh77 <ayoubkhennouci56@gmail.com>
2025-11-13 12:03:32 +0100

    3Commit

--- src/main/kotlin/org/ldv/sporline/controller/MainController.kt
+++ src/main/kotlin/org/ldv/sporline/controller/MainController.kt
@@ -1,4 +1,26 @@
 package org.ldv.sporline.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class MainController {
+
+    @GetMapping("/Sportline")
+    fun home(): String = "index"
+
+    @GetMapping("/Sportline/a-propos")
+    fun aPropos(): String = "pageVisiteur/a-propos"
+
+    @GetMapping("/Sportline/contact")
+    fun contact(): String = "pageVisiteur/contact"
+
+    @GetMapping("/Sportline/inscription")
+    fun inscription(): String = "pageVisiteur/inscription"
+
+    @GetMapping("/Sportline/produits")
+    fun produits(): String = "pageVisiteur/produit"
+
+    @GetMapping("/Sportline/rgpd")
+    fun rgpd(): String = "pageVisiteur/rgpd"
 }
